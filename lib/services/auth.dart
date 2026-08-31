abec49f3269f052b84d12cc858d6853367cc518d
Saleheen Ferdous <mdsaleheenferdous@gmail.com>
2026-09-01 03:09:35 +0600

    Custom User Model

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,19 +1,31 @@
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart' as auth;
+import 'package:brew_crew/models/user.dart';
 
 class AuthService{
 
-  final FirebaseAuth _auth = FirebaseAuth.instance;
-  //sign in anon
+  final auth.FirebaseAuth _auth = auth.FirebaseAuth.instance;
 
+  //create user obj based on Firebase auth User
+  User? _userFromFirebaseUser(auth.User? user){
+    return user != null ? User(uid: user.uid) : null;
+  }
+  //sign in anon
   Future signInAnon() async{
     try {
-      UserCredential result = await _auth.signInAnonymously();
-      User? user = result.user; // User can be null, so we use User?
-      return user;
+      auth.UserCredential result = await _auth.signInAnonymously();
+      auth.User? user = result.user;
+      return _userFromFirebaseUser(user); 
     } catch (e) {
       print(e.toString());
       return null;
     }
+  }
+
+  // auth change user stream
+  Stream<User?> get user {
+    return _auth.authStateChanges()
+      //.map((auth.User? user) => _userFromFirebaseUser(user));
+      .map(_userFromFirebaseUser);
   }
 
  //sign in with email & pass
